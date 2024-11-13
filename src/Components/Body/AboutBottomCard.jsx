2a2b2eed31c2fbf4813f68f738f5ab95bcb29da0
Kejvin Pashaj <kevin.952011@gmail.com>
2024-11-13 18:18:40 +0000

    reward buttons styling

--- src/Components/Body/AboutBottomCard.jsx
+++ src/Components/Body/AboutBottomCard.jsx
@@ -1,18 +1,38 @@
-import Card from "../Card"
+import Card from "../Card";
 
 export default function AboutBottomCard() {
     const aboutCardStyle = {
         textAlign: 'left'
     }
-    const rewardAboutStyle = {
-        textAlign: 'left',
+    const rewardCardsStyle = {
+        ...aboutCardStyle,
         width: '280px',
     }
 
+    const rewardCardsData = [
+        {
+            title: "Bamboo Stand",
+            subTitle: "Pledge $25 or more",
+            content: "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you'll be added to a special Backer member list.",
+            units: 101,
+        },
+        {
+            title: "Black Edition Stand",
+            subTitle: "Pledge $75 or more",
+            content: "You get a Black Special Edition computer stand and a personal thank you. You'll be added to our Backer member list. Shipping is included.",
+            units: 64,
+        },
+        {
+            title: "Mahogany Special Edition",
+            subTitle: "Pledge $200 or more",
+            content: "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You'll be added to our Backer member list. Shipping is included.",
+            units: 0,
+        }
+    ]
 
     return (
-        <Card style={aboutCardStyle}>
-            <div id="about-card">
+        <div id="about-card">
+            <Card style={aboutCardStyle}>
 
                 <h1 className="card-title">About this project</h1>
 
@@ -20,43 +40,27 @@ export default function AboutBottomCard() {
 
                 <p className="card-text">Featuring artisan craftsmanship, the simplicity of design creates extra desk space below your computer to allow notepads, pens, and USB sticks to be stored under the stand.</p>
                 <br />
-                <Card style={rewardAboutStyle}>
-                    <h1 className="reward-card-title">Bamboo Stand</h1>
-                    <h2 className="reward-card-sub">Pledge $25 or more</h2>
 
-                    <p className="card-text">You get an ergonomic stand made of natural bamboo. You&apos;ve helped us launch our promotional campaign, and you’ll be added to a special Backer member list.</p>
+                {rewardCardsData.map((card, i) => {
+                    function checkRewardUnits(noUnit, normal){
+                        return card.units === 0 ? noUnit : normal
+                    }
+                    
+                    return (
+                        <Card style={rewardCardsStyle} key={i}>
+                            <h1 className="reward-card-title">{card.title}</h1>
+                            <h2 className="reward-card-sub">{card.subTitle}</h2>
 
-                    <div className="reward-interaction-box">
-                        <p className="card-text"><span className="reward-unit">101</span> left</p>
-                        <button className="main-btn">Select Reward</button>
-                    </div>
-                </Card>
+                            <p className="card-text">{card.content}</p>
 
-                <Card style={rewardAboutStyle}>
-                    <h1 className="reward-card-title">Black Edition Stand</h1>
-                    <h2 className="reward-card-sub">Pledge $75 or more</h2>
-
-                    <p className="card-text">You get a Black Special Edition computer stand and a personal thank you. You&apos;ll be added to our Backer member list. Shipping is included.</p>
-
-                    <div className="reward-interaction-box">
-                        <p className="card-text"><span className="reward-unit">64</span> left</p>
-                        <button className="main-btn">Select Reward</button>
-                    </div>
-
-                </Card>
-
-                <Card style={rewardAboutStyle}>
-                    <h1 className="reward-card-title">Mahogany Special Edition</h1>
-                    <h2 className="reward-card-sub">Pledge $200 or more</h2>
-
-                    <p className="card-text">You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You&apos;ll be added to our Backer member list. Shipping is included.</p>
-
-                    <div className="reward-interaction-box">
-                        <p className="card-text"><span className="reward-unit">0</span> left</p>
-                        <button className="main-btn">Out of Stock</button>
-                    </div>
-                </Card>
-            </div>
-        </Card>
+                            <div className="reward-interaction-box">
+                                <p className="card-text"><span className="reward-unit">{card.units}</span> left</p>
+                                <button className={checkRewardUnits("disabled-btn", "main-btn")}>{checkRewardUnits("Out of Stock", "Select Reward")}</button>
+                            </div>
+                        </Card>
+                    )
+                })}
+            </Card>
+        </div>
     )
 }
--- src/Components/Body/MainTopCard.jsx
+++ src/Components/Body/MainTopCard.jsx
@@ -10,19 +10,21 @@ export default function MainTopCard() {
 
   return (
     <Card>
-      <img src={mastercraftLogo} style={imgStyle} />
-      <div style={{ marginTop: '-60px' }}>
-        <h1 className="card-title">
-          Mastercraft Bamboo Monitor Riser
-        </h1>
+      <div id="main-card">
+        <img src={mastercraftLogo} style={imgStyle} />
+        <div style={{ marginTop: '-60px' }}>
+          <h1 className="card-title">
+            Mastercraft Bamboo Monitor Riser
+          </h1>
 
-        <p className="card-text">
-          A beautiful & handcrafted monitor stand to reduce neck and eye strain.
-        </p>
+          <p className="card-text">
+            A beautiful & handcrafted monitor stand to reduce neck and eye strain.
+          </p>
 
-        <div className="top-buttons">
-          <button className="main-btn">Back this project</button>
-          <button className="bookmark-btn"><img src={bookmarkIcon}/></button>
+          <div className="top-buttons">
+            <button className="main-btn">Back this project</button>
+            <button className="bookmark-btn"><img src={bookmarkIcon} /></button>
+          </div>
         </div>
       </div>
     </Card>
